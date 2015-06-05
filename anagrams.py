1711a60d0110dde69be4017690229439e0a0c115
Hugh Brown <hbrown@amplify.com>
2015-06-05 14:40:57 -0600

    Write better anagram code

--- anagrams.py
+++ anagrams.py
@@ -1,43 +1,13 @@
 #CHALLENGE - given a list of words, return the words that are anagrams of other words within the same list
 
-#version 1
-def anagrams(input):
-    output = []
-    output2 = []
-    for i in input:
-        word1 = [c for c in i]
-        word1.sort()
-        for j in input:
-            word2 = [c for c in j]
-            word2.sort()
-            if word1 == word2 and i != j:
-                output2.extend([i,j])
-    for k in output2:
-        if k not in output:
-            output.append(k)
-    return output
+from collections import defaultdict
 
-# version 2
-def ana(word, words):
-    a = "".join(sorted(word))
-    anas = []
-    for i in words:
-        if "".join(sorted(i)) == a:
-            anas.append(i)
-    return anas
+def anagrams(words):
+    d = defaultdict(list)
+    for word in words:
+        key = "".join(sorted(word))
+        d[key].append(word)
+    return [v for v in d.values() if len(v) > 1]
 
-
-# version 3
-def ana(lst):
-    results=set()
-    for word in lst:
-        clone = [cloneword for cloneword in lst]
-        asdf = sorted(word.lower())
-        clone.remove(word)
-        #print clone
-        for word2 in clone:
-            asdf2 = sorted(word2.lower())
-            if asdf == asdf2:
-                results.add(word)
-                results.add(word2)
-    return results
+if __name__ == '__main__':
+    print(anagrams(['race', 'acre', 'care', 'fool', 'spine', 'pines', 'snipe', 'spite']))
